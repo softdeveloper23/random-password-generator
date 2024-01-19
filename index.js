bd43688e5164e8706f00131b848398f29e65513b
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-19 09:57:18 +0100

    Added code for variables and basic function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Random Password Generator
+function generatePassword(length, uppercase, lowercase, numbers, symbols) {
+    
+    return "";
+}
+
+const passwordLength = 12;
+const includeUppercase = true;
+const includeLowercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`)

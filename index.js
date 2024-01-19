b9b2e63d9a6a923328fd92eb7c1f4dca5ad75cd0
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-19 10:13:38 +0100

    Finished the application

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 // Random Password Generator
 function generatePassword(length, uppercase, lowercase, numbers, symbols) {
-    
-    return "";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if (length <= 0) {
+        return `(Error) Password length must be greater than 0`;
+    }
+    if (allowedChars.length <= 0) {
+        return `(Error) You must select at least one character type`;
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+    return password;
 }
 
 const passwordLength = 12;
